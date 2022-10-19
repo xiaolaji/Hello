109b52b7707b9d8a212cb9abf86a77b7c20d5bd8
yi <281232910@qq.com>
2022-10-19 18:07:05 +0800

    first

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -15,7 +15,13 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         titleView.text = "我们是爱学习"
     }
-
-
+    
+    /// 按钮事件的回调方法
+    /// - Parameter sender:
+    @IBAction func buttonClick(_ sender: UIButton) {
+        print("点我了")
+        sender.setTitle("dianjile", for: .normal)
+    }
+    
 }
 
