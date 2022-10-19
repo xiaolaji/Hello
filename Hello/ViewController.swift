2fd8219f0f2a00011c7a81d204702108f1b86f05
yi <281232910@qq.com>
2022-10-19 19:23:21 +0800

    target

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -10,10 +10,13 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var titleView: UILabel!
+    @IBOutlet weak var buttonView: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         titleView.text = "我们是爱学习"
+        buttonView.addTarget(self, action: #selector(buttonClick2(_:)), for: .touchUpInside)
     }
     
     /// 按钮事件的回调方法
@@ -21,6 +24,11 @@ class ViewController: UIViewController {
     @IBAction func buttonClick(_ sender: UIButton) {
         print("点我了")
         sender.setTitle("dianjile", for: .normal)
+    }
+    
+    @objc func buttonClick2(_ sender: UIButton) {
+        print("点我了22222")
+        
     }
     
 }
