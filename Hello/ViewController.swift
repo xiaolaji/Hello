ec5a79886e820679a94dc3ff2cd9c27a56f32b6e
yi <281232910@qq.com>
2022-10-19 17:33:01 +0800

    first

---
+++ Hello/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by yi on 2022/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var titleView: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        titleView.text = "我们是爱学习"
+    }
+
+
+}
+
